887da7a63ff1871cfb825651340ad89418c0c04f
Ninganagouda V K <ninganagoudavk@Ninganagoudas-MacBook-Air.local>
2024-08-20 11:12:34 +0530

    initial commit

---
+++ src/apis/index.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Use environment variables for the base URL
+  timeout: 10000, // Optional: Set a timeout for requests
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+      // Add authorization token or other headers here
+     console.log('configg',config)
+      return config;
+    },
+    (error) => {
+      // Handle request error here
+      return Promise.reject(error);
+    }
+  );
+  
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // Handle responses globally
+    
+    return response;
+  },
+  (error) => {
+    // Handle errors globally
+    if (error.response && error.response.status === 401) {
+      // Handle 401 Unauthorized errors (e.g., redirect to login)
+      console.log('console',error.response)
+      window.location.href = '/';
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+export default axiosInstance
+
+
+export const getRecipies=(params:any)=>axiosInstance.get('v2',{
+    params
+})
---
+++ src/app/recipes/[sulg]/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import  { getRecipies } from "@/apis/index";
+import { useEffect, useState } from "react";
+
+ 
+    const Recipies =  () => {
+        const [recipes,setRecipies]=useState([])
+
+        const getData=async()=>{
+            const response = await  getRecipies({
+                type:'public',
+                app_id:"b72bb112",
+                app_key:'c4615d10a88089afc5050da5f9f84f18',
+                q:'q'
+            })
+            setRecipies(response.data?.hits)
+          console.log('response',)
+        }
+
+        useEffect(()=>{
+            // getData()
+        },[])
+
+        console.log('recipes',recipes)
+    
+  return (
+    <div>
+   
+   {recipes?.length?recipes.map((item:any)=><img src={item?.recipe?.image}/>):''}
+    </div>
+  )
+}
+
+export default Recipies
